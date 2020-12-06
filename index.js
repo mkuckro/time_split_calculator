5884bfd488044032240e31447b1bc8d1a03c21c0
PJ <pbrown@luc.id>
2020-12-06 17:40:10 -0600

    some suggestions

--- index.js
+++ index.js
@@ -1,57 +1,62 @@
 // access user inputs from form
 const formInput = document.querySelector('#input-form');
-const inputType = document.querySelector('#time-split');
-const inputMinutesSelector = document.querySelector('#user-input-minutes');
-const inputSecondsSelector = document.querySelector('#user-input-seconds');
 
-// functionality for when "calculate" button is clicked
-formInput.addEventListener('submit', function(e) {
-  // store value for input type (time or split) to type variable
-  const type = inputType.value;
+const convertSecondsToMinutes = (seconds) => seconds / 60;
 
-  // store user inputs for minutes and seconds converted to number data types to variables
-  const inputMinutes = Number(inputMinutesSelector.value);
-  const inputSeconds = Number(inputSecondsSelector.value);
+const calculateRemainingSeconds = (outputSeconds, outputMinutes, secondsResult) =>
+  (outputSeconds + secondsResult) - (outputMinutes * 60);
 
-  // calculate total seconds from minute and second inputs
-  const totalInputSeconds = (inputMinutes * 60) + inputSeconds;
+const getTotalInputSeconds = () => {
+  const inputMinutes = Number(document.querySelector('#user-input-minutes').value);
+  const inputSeconds = Number(document.querySelector('#user-input-seconds').value);
 
-  // prevents default form submit action
-  e.preventDefault();
+  return (inputMinutes * 60) + inputSeconds;
+}
 
+const calculateOutput = (timeSplitType, totalInputSeconds) => {
   // split input: 120 seconds / 500 meters = x / 825
   // time input: 300 seconds / 825 meters = x / 500 meters
-  // initialize output variable
-  let outputSeconds = 0;
-  // assign calculated output in seconds to outputSeconds variable depending on whether the user input was time or split
-  if (type === 'time') {
-    outputSeconds = (totalInputSeconds / 825) * 500;
-  } else if (type === 'split') {
-    outputSeconds = (totalInputSeconds / 500) * 825;
-  }
+  let outputSeconds = timeSplitType === 'time' ? (totalInputSeconds / 825) * 500 : (totalInputSeconds / 500) * 825;
 
-  // convert outputSeconds to minutes for display
-  const outputMinutes = outputSeconds / 60;
-  // convert output minutes integer to seconds
-  const secondsResult = (outputMinutes - Math.floor(outputMinutes)) * 60;
-  // calculate seconds remaining from outputMinutes and add to remaining seconds
-  const remainingSeconds = (outputSeconds + secondsResult) - (outputMinutes * 60);
-
-  let displayResult;
-  // display leading 0 if the remaining seconds are less than 10 and no leading 0 if greater than 10
-  if (remainingSeconds < 10) {
-    displayResult = `${Math.floor(outputMinutes)}:0${remainingSeconds.toFixed(1)}`;
-  } else {
-    displayResult = `${Math.floor(outputMinutes)}:${remainingSeconds.toFixed(1)}`;
+  return {
+    outputSeconds,
+    outputMinutes: convertSecondsToMinutes(outputSeconds)
   }
+}
+
+const showCalculatedResult = (timeSplitType, remainingSeconds, outputMinutes) => {
+  const displayResult = getDisplayResult(outputMinutes, remainingSeconds);
 
   // access the display result element
-  const calculatedResultDisplay = document.querySelector('#calculated-result');
-
+  const calculatedResultDiv = document.querySelector('#calculated-result');
   // set the calculated result as the display HTML text 
-  if (type === 'time') {
-    calculatedResultDisplay.innerHTML = `Your ${'split'.bold()} is ${displayResult}/500m`;
-  } else if (type === 'split') {
-    calculatedResultDisplay.innerHTML = `Your ${'time'.bold()} is ${displayResult}/825m`;
+  if (timeSplitType === 'time') {
+    calculatedResultDiv.innerHTML = `Your ${'split'.bold()} is ${displayResult}/500m`;
+  } else if (timeSplitType === 'split') {
+    calculatedResultDiv.innerHTML = `Your ${'time'.bold()} is ${displayResult}/825m`;
   }
+
+}
+
+// display leading 0 if the remaining seconds are less than 10 and no leading 0 if greater than 10
+const getDisplayResult = (outputMinutes, remainingSeconds) =>
+  remainingSeconds < 10
+    ? `${Math.floor(outputMinutes)}:0${remainingSeconds.toFixed(1)}`
+    : `${Math.floor(outputMinutes)}:${remainingSeconds.toFixed(1)}`;
+
+
+formInput.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const timeSplitType = document.querySelector('#time-split').value;
+
+  const totalInputSeconds = getTotalInputSeconds()
+
+  const { outputSeconds, outputMinutes } = calculateOutput(timeSplitType, totalInputSeconds)
+
+  // convert output minutes integer to seconds
+  const secondsResult = (outputMinutes - Math.floor(outputMinutes)) * 60;
+  const remainingSeconds = calculateRemainingSeconds(outputSeconds, outputMinutes, secondsResult);
+
+  showCalculatedResult(timeSplitType, remainingSeconds, outputMinutes)
 });
